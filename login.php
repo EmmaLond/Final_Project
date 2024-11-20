815f3fa8f37c16d864c66ae1d18515d5ef682daf
EmmaLond <147564243+EmmaLond@users.noreply.github.com>
2024-11-19 21:23:48 -0500

    pulled login code from demo

--- login.php
+++ login.php
@@ -31,6 +31,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body>
     <div class= "auth-container">
         <h1>Login Here</h1>
+        <?php if ($error_message): ?>
+            <div class="error"><?php echo htmlspecialchars($error_message); ?></div>
+        <?php endif; ?>
         
+        <form method="POST" action="" class="auth-form">
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit" name="login">Login</button>
+        </form>
+        <p>Don't have an account? <a href="register.php">Register here</a></p>
     </div>
 </body>
