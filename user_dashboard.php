73d4fc88274ad019af03c2e804dbd2307ec8220b
EmmaLond <147564243+EmmaLond@users.noreply.github.com>
2024-11-22 20:27:31 -0500

    user wip

--- user_dashboard.php
+++ user_dashboard.php
@@ -1,1 +1,10 @@
+<?php
+include('config.php');
 
+session_start();
+
+//Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php'); // Redirect to login if not logged in
+    exit;
+}
