6f8f7cb22984bbd9921e8216f016d6805a0889d1
EmmaLond <147564243+EmmaLond@users.noreply.github.com>
2024-12-04 18:57:52 -0500

    copy back in styles

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instride</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="stylesF.css">
 </head>
 <body>
     <header>
