b3dd57e08b681e26fdfec9e643f7da740ec37312
EmmaLond <147564243+EmmaLond@users.noreply.github.com>
2024-12-10 22:25:03 -0500

    wip

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instride</title>
-    <link rel="stylesheet" href="stylesS.css">
+    <link rel="stylesheet" href="stylesSS.css">
 </head>
 <body>
     <header>
